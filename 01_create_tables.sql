b723799ec33207065d576035a811a9def116c147
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-15 21:08:36 +0700

    update create table

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -7,6 +7,7 @@ create table barang(
     nama_barang varchar(41),
     tipe varchar(4),
     nama_tipe varchar(11),
+    kode_lini int,
     lini varchar(8),
     kemasan varchar(6)
 );
@@ -28,7 +29,7 @@ create table pelanggan(
     id_cabang_sales varchar(5),
     cabang_sales varchar(9),
     id_group varchar(3),
-    group varchar(6)
+    grup varchar(6)
 );
 -- table pelanggan_ds
 create table pelanggan_ds(
@@ -38,7 +39,7 @@ create table pelanggan_ds(
     id_cabang_sales varchar(5),
     cabang_sales varchar(9),
     id_distributor varchar(3),
-    group varchar
+    grup varchar(6)
 );
 -- table penjualan
 create table penjualan(
@@ -50,7 +51,7 @@ create table penjualan(
     id_barang varchar(7),
     jumlah_barang int,
     unit varchar(3),
-    harga decimal(7, 2),
+    harga int,
     mata_uang varchar(3),
     brand_id varchar(7),
     lini varchar(8)
@@ -63,12 +64,29 @@ create table penjualan_ds(
     id_barang varchar(7),
     jumlah_barang int,
     unit varchar(6),
-    harga decimal(7, 2),
+    harga int,
     mata_uang varchar(3)
 );
--- Import csv data to the database
+-- import data csv lalu masukkan kedalam table di database
 -- Make sure dataset path (in local storage) must be complete until the file_name.csv
-COPY penjualan(
+-- COPY DATA PENJUALAN
+COPY penjualan (
+    id_distributor,
+    id_cabang,
+    id_invoice,
+    tanggal,
+    id_customer,
+    id_barang,
+    jumlah_barang,
+    unit,
+    harga,
+    mata_uang,
+    brand_id,
+    lini
+)
+FROM 'C:\Users\bagaswara\Documents\VIX\Kimia_Farma_Sales_Analysis\datasets\penjualan.csv' DELIMITER ',' CSV HEADER;
+-- COPY DATA PENJUALAN_DS
+COPY penjualan_ds(
     id_invoice,
     tanggal,
     id_customer,
@@ -77,9 +95,21 @@ COPY penjualan(
     unit,
     harga,
     mata_uang
-) -- C:\Users\bagaswara\Documents\VIX\Kimia_Farma_Sales_Analysis\penjualan.csv
-FROM 'E:\Rakamin\JAP\Big Data Analytics - Kimia Farma\penjualan.csv' DELIMITER ',' CSV HEADER;
+)
+FROM 'C:\Users\bagaswara\Documents\VIX\Kimia_Farma_Sales_Analysis\datasets\penjualan_ds.csv' DELIMITER ',' CSV HEADER;
+-- COPY DATA PELANGGAN
 COPY pelanggan(
+    id_customer,
+    level,
+    nama,
+    id_cabang_sales,
+    cabang_sales,
+    id_group,
+    grup
+)
+FROM 'C:\Users\bagaswara\Documents\VIX\Kimia_Farma_Sales_Analysis\datasets\pelanggan.csv' DELIMITER ',' CSV HEADER;
+-- COPY DATA PELANGGAN_DS
+COPY pelanggan_ds(
     id_customer,
     level,
     nama,
@@ -88,8 +118,21 @@ COPY pelanggan(
     id_distributor,
     grup
 )
-FROM 'E:\Rakamin\JAP\Big Data Analytics - Kimia Farma\pelanggan.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\bagaswara\Documents\VIX\Kimia_Farma_Sales_Analysis\datasets\pelanggan_ds.csv' DELIMITER ',' CSV HEADER;
+-- COPY DATA TABLE BARANG
 COPY barang(
+    kode_barang,
+    sektor,
+    nama_barang,
+    tipe,
+    nama_tipe,
+    kode_lini,
+    lini,
+    kemasan
+)
+FROM 'C:\Users\bagaswara\Documents\VIX\Kimia_Farma_Sales_Analysis\datasets\barang.csv' DELIMITER ',' CSV HEADER;
+-- COPY DATA TABLE BARANG_DS
+COPY barang_ds(
     kode_barang,
     nama_barang,
     kemasan,
@@ -98,4 +141,4 @@ COPY barang(
     kode_brand,
     brand
 )
-FROM 'E:\Rakamin\JAP\Big Data Analytics - Kimia Farma\barang.csv' DELIMITER ',' CSV HEADER;
+FROM 'C:\Users\bagaswara\Documents\VIX\Kimia_Farma_Sales_Analysis\datasets\barang_ds.csv' DELIMITER ',' CSV HEADER;
