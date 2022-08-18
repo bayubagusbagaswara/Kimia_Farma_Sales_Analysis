4be08b443df218c56860fe3fbac26678a053fe4f
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-18 11:13:07 +0700

    update base table pelanggan_dim

--- 09_base_table_pelanggan_dim.sql
+++ 09_base_table_pelanggan_dim.sql
@@ -4,7 +4,6 @@ create table pelanggan_dim (
     nama varchar(30),
     id_cabang_sales varchar(5),
     cabang_sales varchar(10),
-    id_distributor varchar(3),
     group varchar(10),
     primary key (id_customer)
 );
