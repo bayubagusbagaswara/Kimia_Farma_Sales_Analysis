c11e305ab4c6173d96ce363ee47c869f2d4ebe06
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-18 16:16:15 +0700

    add create table penjualan

---
+++ 03_create_table_penjualan.sql
@@ -0,0 +1,14 @@
+create table penjualan(
+    id_distributor varchar(3),
+    id_cabang varchar(5),
+    id_invoice varchar(6),
+    tanggal date,
+    id_customer varchar(9),
+    id_barang varchar(7),
+    jumlah_barang int,
+    unit varchar(3),
+    harga int,
+    mata_uang varchar(3),
+    brand_id varchar(7),
+    lini varchar(8)
+);
