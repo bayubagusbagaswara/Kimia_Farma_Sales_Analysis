2d0dc7053c57fd4c86c5fe79fe83eb00bd3130c2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-18 11:13:34 +0700

    update base table barang_dim

--- 10_base_table_barang_dim.sql
+++ 10_base_table_barang_dim.sql
@@ -1,10 +1,10 @@
 create table barang_dim (
     id_barang varchar(9),
     nama_barang varchar(50),
+    sektor varchar(1),
+    tipe varchar(11),
+    brand varchar(10),
     kemasan varchar(6),
     harga int,
-    nama_tipe varchar(5),
-    kode_brand int,
-    brand varchar(8),
     primary key (id_barang)
 );
