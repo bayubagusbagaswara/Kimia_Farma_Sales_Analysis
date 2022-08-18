8e35fec5d33c99e19f0f5510f85e7f158505f3ba
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-18 21:13:18 +0700

    add create table pelanggan_dim

--- 09_pelanggan_dim.sql
+++ 09_pelanggan_dim.sql
@@ -2,6 +2,7 @@ create table pelanggan_dim (
     id_customer varchar(9),
     `level` varchar(10),
     nama varchar(30),
+    id_cabang_sales varchar(5),
     cabang_sales varchar(10),
     `group` varchar(10),
     primary key (id_customer)
