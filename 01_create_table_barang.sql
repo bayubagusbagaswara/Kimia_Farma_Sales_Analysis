47879fa7cc1da089888768f3090808d36b31cb21
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-18 16:12:42 +0700

    add create table barang

---
+++ 01_create_table_barang.sql
@@ -0,0 +1,10 @@
+create table barang (
+    kode_barang varchar(7),
+    kemasan varchar(6),
+    kode_lini int,
+    lini varchar(8),
+    nama_barang varchar(41),
+    nama_tipe varchar(11),
+    sektor varchar(1),
+    tipe varchar(4)
+);
