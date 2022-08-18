81555921632365a6ac5fbc71a4bd0bf01c5693a4
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-18 21:12:57 +0700

    add create table barang_dim

--- 08_barang_dim.sql
+++ 08_barang_dim.sql
@@ -1,9 +1,7 @@
 create table barang_dim (
     id_barang varchar(9),
     nama_barang varchar(50),
-    sektor varchar(1),
-    tipe varchar(11),
-    brand varchar(10),
+    lini varchar(10),
     kemasan varchar(6),
     harga int,
     primary key (id_barang)
