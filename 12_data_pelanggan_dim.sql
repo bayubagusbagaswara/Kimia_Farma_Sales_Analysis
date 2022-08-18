13b15cbdedf0604151cc8a37f062c22791f5452b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-18 21:13:56 +0700

    add create table data pelanggan_dim

--- 12_data_pelanggan_dim.sql
+++ 12_data_pelanggan_dim.sql
@@ -1,7 +1,10 @@
+-- data table pelanggan_dim kita ambil dari table pelanggan
+-- data yang kita perlukan adalah id_customer, level, nama, id_cabang_sales, cabang_sales, group
 insert into pelanggan_dim
 select distinct id_customer,
     `level`,
     nama,
+    id_cabang_sales,
     cabang_sales,
     `group`
 from pelanggan;
