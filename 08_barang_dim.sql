71a466fe61850b2ebb872375f18054ab82237eba
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-19 14:49:49 +0700

    update table barang_dim

--- 08_barang_dim.sql
+++ 08_barang_dim.sql
@@ -1,7 +1,8 @@
+-- kolom barang_dim yang akan di aggregate ke table penjualan_fact adalah id_barang, nama_barang, brand (nama_brand), harga
 create table barang_dim (
     id_barang varchar(9),
     nama_barang varchar(50),
-    lini varchar(10),
+    brand varchar(10),
     kemasan varchar(6),
     harga int,
     primary key (id_barang)
