aae693b2fa946b58f75fdc969f80de6991f4739b
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-19 15:28:46 +0700

    add insert data penjualan_fact

---
+++ 12_insert_data_penjualan_fact.sql
@@ -0,0 +1,21 @@
+insert into penjualan_fact
+select CONCAT(p.id_invoice, pd.id_customer) as id_penjualan,
+    p.id_invoice,
+    p.tanggal,
+    pd.id_customer,
+    pd.level,
+    pd.nama as nama_customer,
+    pd.id_cabang_sales,
+    pd.cabang_sales as nama_cabang_sales,
+    pd.id_distributor,
+    pd.group,
+    bd.id_barang,
+    bd.nama_barang,
+    bd.brand as nama_brand,
+    bd.harga as harga_per_kemasan,
+    p.jumlah_barang,
+    p.unit,
+    p.jumlah_barang * bd.harga as total_penjualan
+FROM penjualan as p
+    JOIN barang_dim as bd ON p.id_barang = bd.id_barang
+    JOIN pelanggan_dim as pd ON p.id_customer = pd.id_customer;
