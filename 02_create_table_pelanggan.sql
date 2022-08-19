c9aad5299f325379aa040e2638d8f8360f95d52c
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-19 15:14:02 +0700

    update create table pelanggan

--- 02_create_table_pelanggan.sql
+++ 02_create_table_pelanggan.sql
@@ -1,9 +1,9 @@
 create table pelanggan (
     id_customer varchar(9),
-    cabang_sales varchar(9),
-    `group` varchar(6),
-    id_cabang_sales varchar(5),
-    id_group varchar(3),
     level varchar(7),
-    nama varchar(17)
+    nama varchar(17),
+    id_cabang_sales varchar(5),
+    cabang_sales varchar(9),
+    id_distributor varchar(3),
+    `group` varchar(6)
 );
