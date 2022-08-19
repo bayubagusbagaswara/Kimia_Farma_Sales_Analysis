41c0645c5dd8e7b786dc29aa9367dc77f6839728
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-19 15:26:55 +0700

    update insert data barang_dim

---
+++ 09_insert_data_barang_dim.sql
@@ -0,0 +1,9 @@
+-- data table barang_dim kita ambil dari data table barang
+-- data yang kita perlukan adalah kode_barang, nama_barang, lini, kemasan, dan harga
+insert into barang_dim
+select distinct kode_barang,
+    nama_barang,
+    lini as brand,
+    kemasan,
+    harga
+from barang;
