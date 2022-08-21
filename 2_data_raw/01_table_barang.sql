b0e20fcdc6df6bd19a6211f7473dcd2f55974692
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-21 13:06:53 +0700

    Data Raw

---
+++ 2_data_raw/01_table_barang.sql
@@ -0,0 +1,10 @@
+create table `barang` (
+    `kode_barang` varchar(7),
+    `sektor` varchar(1),
+    `nama_barang` varchar(41),
+    `tipe` varchar(4),
+    `nama_tipe` varchar(11),
+    `kode_lini` int(11),
+    `lini` varchar(8),
+    `kemasan` varchar(6)
+);
