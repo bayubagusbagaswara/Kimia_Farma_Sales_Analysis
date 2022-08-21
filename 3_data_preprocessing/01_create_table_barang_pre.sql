8e2531e7eb0263227aba6de03a4dfd9d545c6350
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-21 14:32:16 +0700

    update create table barang_pre

--- 3_data_preprocessing/01_create_table_barang_pre.sql
+++ 3_data_preprocessing/01_create_table_barang_pre.sql
@@ -1,12 +1,10 @@
--- kita akan join table barang dan table barang_ds
--- kita hanya membutuhkan data id_barang, nama, kemasan, harga, kode_brand, nama_brand, nama_tipe
-create table barang_pre (
-    id_barang varchar(7),
-    nama varchar(41),
-    nama_tipe varchar(11),
-    kemasan varchar(6),
-    harga int,
-    kode_brand int,
-    nama_brand varchar(8),
+create table `barang_pre` (
+    `id_barang` varchar(7),
+    `nama` varchar(41),
+    `nama_tipe` varchar(11),
+    `kemasan` varchar(6),
+    `harga` int,
+    `kode_brand` int,
+    `nama_brand` varchar(8),
     primary key (id_barang)
 );
