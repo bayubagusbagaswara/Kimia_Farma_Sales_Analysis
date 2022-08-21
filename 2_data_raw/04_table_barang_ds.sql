3d73507051989214d416fe6db7c0477fd1ba7b73
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-08-21 14:30:41 +0700

    update table barang_ds

--- 2_data_raw/04_table_barang_ds.sql
+++ 2_data_raw/04_table_barang_ds.sql
@@ -1,9 +1,10 @@
-CREATE TABLE `barang_ds` (
+create table `barang_ds` (
     `kode_barang` varchar(7),
     `nama_barang` varchar(41),
     `kemasan` varchar(6),
-    `harga` int(11),
+    `harga` int,
     `nama_tipe` varchar(11),
-    `kode_brand` int(11),
-    `brand` varchar(8)
+    `kode_brand` int,
+    `brand` varchar(8),
+    primary key (kode_barang)
 );
